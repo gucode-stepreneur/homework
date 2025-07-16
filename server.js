c53ab4875a5626bb18ca222cf71d0f952b409add
Stepreneur <kfranzaza13@gmail.com>
2025-07-16 17:16:03 +0700

    port env

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const prisma = new PrismaClient();
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
