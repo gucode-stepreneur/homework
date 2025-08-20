9ba1ce1f80faa7841ec250803f3eda336dc36908
Stepreneur <kfranzaza13@gmail.com>
2025-08-20 13:32:43 +0700

    change to no date

--- server.js
+++ server.js
@@ -79,12 +79,12 @@ async function checkHomework() {
     // 2 วัน = 2880 นาที, 1 วัน = 1440 นาที
     if (!hw.beforetwodaynoti && diffMinutes <= 2880 + tolerance && diffMinutes >= 2880 - tolerance) {
       // แจ้งเตือน 2 วัน (±5 นาที)
-      message2 += `• ${hw.title} (${hw.subject}) - ${hw.dueDate}\n👤 ${hw.user.username}\n\n`;
+      message2 += `• ${hw.title} (${hw.subject}) - \n👤 ${hw.user.username}\n\n`;
       ids2.push(hw.id);
     }
     if (!hw.beforeonedaynoti && diffMinutes <= 1440 + tolerance && diffMinutes >= 1440 - tolerance) {
       // แจ้งเตือน 1 วัน (±5 นาที)
-      message1 += `• ${hw.title} (${hw.subject}) - ${hw.dueDate}\n👤 ${hw.user.username}\n\n`;
+      message1 += `• ${hw.title} (${hw.subject}) - \n👤 ${hw.user.username}\n\n`;
       ids1.push(hw.id);
     }
   });
